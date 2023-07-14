fea8158d1dd4cffd133dd7b50f6c4d1180066ee0
Praveen Singh Rawat <tech.rawat99@gmail.com>
2023-07-14 23:31:17 +0530

    while loop

--- arrays.java
+++ arrays.java
@@ -2,8 +2,10 @@ public class arrays{
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8,9,0};
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(" "+arr[i]);
+        int i=0;
+        while(i<arr.length){
+            System.out.print("  "+arr[i]);
+            i++;
         }
     }
 }
