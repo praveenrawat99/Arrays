d8eaba7bcce64a1020207c9d65ba91c33ee18db3
Praveen Singh Rawat <tech.rawat99@gmail.com>
2023-07-24 12:48:02 +0530

    three sum equal to target

--- arrays.java
+++ arrays.java
@@ -1,37 +1,42 @@
-import java.util.HashSet;
+import java.util.Arrays;
 
 public class arrays{
-    public static void three_sum(int arr[]){
-        boolean found = true;
+    public static void three_sum(int arr[],int tar){
+        Arrays.sort(arr);
+        boolean found=false;
         for(int i=0;i<arr.length-1;i++){
-            HashSet<Integer> set=new HashSet<>();
-            for(int j=i+1;j<arr.length;j++){
-                int X=-(arr[i]+arr[j]);
-                if(set.contains(X)){
-                    System.out.println("Triplet is: "+arr[i]+" , "+arr[j]+" , "+X);
+            int left=i+1;
+            int right=arr.length-1;
+            int X=arr[i];
+
+            while(left<right){
+                if(arr[left]+arr[right]+X==tar){
+                    System.out.print(X+"  ");
+                    System.out.print(arr[left]+"  ");
+                    System.out.println(arr[right]+"  ");
+
+                    left++;
+                    right--;
                     found=true;
+                }
+                else if(arr[left]+arr[right]+X<tar){
+                    left++;
                 }else{
-                    set.add(arr[j]);
+                    right--;
                 }
             }
         }
-        if(found==false){
-            System.out.println("no triplet");
+        if(found!=true){
+            System.out.println("no triplets equal to target");
         }
     }
     public static void main(String[] args){
-        int arr[]={-1,0,1,2,-1,-4};
-        three_sum(arr);
+        int arr[]={-3,0,1,2,-1,4};
+        three_sum(arr, 2);
     }
 }
 
 
-/*for this question 
- * we have to find the triplets with zero sum
- * so we run a loop from int i=0 to i=arr.length-2
- * we will implement a HashSet 
- * after that we run a second nested loop from j=i+1 to j=arr.length-1
- * after this we check a condition int X=-(arr[i]+arr[j])
- * if X is already present in the set so we will get triplets with zero sum 
- * if not so we will add arr[j] in the set
- */
+/*
+ * here we used three pointer approach
+*/
