1abd1f0776c1a309a38ec80f9744796a8e01911b
Praveen Singh Rawat <tech.rawat99@gmail.com>
2023-07-24 12:59:16 +0530

    pair in a rotated sorted array

--- arrays.java
+++ arrays.java
@@ -1,26 +1,46 @@
 public class arrays {
-    public static int min(int arr[], int si, int ei) {
-        while (si < ei) {
-            int mid = si + (ei - si) / 2;
-            if (arr[mid] > arr[mid + 1]) {
-                return arr[mid];
-            } else if (arr[mid] < arr[mid - 1]) {
-                return arr[mid - 1];
-            } else if (arr[mid] < arr[si]) {
-                ei = mid - 1;
-            } else {
-                si = mid + 1;
-            }
+    public static boolean pair_sum(int arr[], int n, int x) {
+        // Find the pivot element
+        int i;
+        for (i = 0; i < n - 1; i++)
+            if (arr[i] > arr[i + 1])
+                break;
+
+        // l is now index of smallest element
+        int l = (i + 1) % n;
+
+        // r is now index of largest element
+        int r = i;
+
+        // Keep moving either l or r till they meet
+        while (l != r) {
+            // If we find a pair with sum x, we
+            // return true
+            if (arr[l] + arr[r] == x)
+                return true;
+
+            // If current pair sum is less, move
+            // to the higher sum
+            if (arr[l] + arr[r] < x)
+                l = (l + 1) % n;
+
+            // Move to the lower sum side
+            else
+                r = (n + r - 1) % n;
         }
-        return arr[si];
+        return false;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 6, 7, 8, 1, 2 };
-        System.out.println(min(arr, 0, arr.length - 1));
+        int arr[] = { 11, 15, 6, 8, 9, 10 };
+        if (pair_sum(arr, arr.length, 25)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
 
 /*
- * we use binary search here
+ * here we used two pointer approach
  */
