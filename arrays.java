708210fa58481bde1dbaff2df022b98fd80a9934
Praveen Singh Rawat <tech.rawat99@gmail.com>
2023-08-17 20:54:51 +0530

    print subarrays

--- arrays.java
+++ arrays.java
@@ -1,22 +1,22 @@
 public class arrays{
-    public static void pairs(int arr[]){
-        int total_pairs=0;
+    public static void print_subarrays(int arr[]){
+        int total_subarrays=0;
         for(int i=0;i<arr.length;i++){
-            int curr=arr[i];
-            for(int j=i+1;j<arr.length;j++){
-                System.out.print("("+curr + ',' + arr[j]+")");
-                total_pairs++;
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(arr[k]+"  ");
+                    total_subarrays++;
+                }
+                System.out.println();
             }
             System.out.println();
         }
-        System.out.println("total pairs = " + total_pairs);
+        System.out.println("total subarrays = " + total_subarrays);
     }
     public static void main(String[] args){
-        int [] arr = {2,4,6,8,10};
-        pairs(arr);
+        int arr[]={2,4,6,8,10};
+        print_subarrays(arr);
     }
 }
-
-/*
- * here we used nested loops
- */
