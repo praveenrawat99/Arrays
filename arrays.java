7a1dc21039875bfbec477ecc7aa12cda245d724e
Praveen Singh Rawat <tech.rawat99@gmail.com>
2023-08-17 20:25:33 +0530

    pairs in an array

--- arrays.java
+++ arrays.java
@@ -1,26 +1,22 @@
 public class arrays{
-    public static int binary_Search(int arr[],int start,int end,int key){
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(mid==key){
-                return mid;
-            }else if(key<mid){
-                end=mid-1;
-            }else{
-                start=mid+1;
+    public static void pairs(int arr[]){
+        int total_pairs=0;
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+curr + ',' + arr[j]+")");
+                total_pairs++;
             }
+            System.out.println();
         }
-        return -1;
+        System.out.println("total pairs = " + total_pairs);
     }
-    public static void main(String [] args){
-        int arr[]={0,1,2,3,4,5,6,7};
-        System.out.println("Element found at index = " + binary_Search(arr, 0, arr.length-1, 7));
+    public static void main(String[] args){
+        int [] arr = {2,4,6,8,10};
+        pairs(arr);
     }
 }
 
-
-
-
 /*
- * here we used binary search algorithm
+ * here we used nested loops
  */
